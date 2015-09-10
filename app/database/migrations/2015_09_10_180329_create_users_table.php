bc8b74c71734cdc96097ccbc37a51ea81f08d2a6
Dylan Perenchio <perenchiod@gmail.com>
2015-09-10 14:08:51 -0500

    First commit

---
+++ app/database/migrations/2015_09_10_180329_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users_table', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('email');
+			$table->string('password');
+			$table->timestamps();
+			$table->rememberToken();
+            $table->softdeletes();
+
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users_table');
+	}
+
+}
